57035afad6c866da974dce775f409802aa9b2555
agil999 <115994499+agil999@users.noreply.github.com>
2022-11-16 15:12:09 +0700

    Update game-tebakbendera_hint.js

--- plugins/game-tebakbendera_hint.js
+++ plugins/game-tebakbendera_hint.js
@@ -1,14 +1,10 @@
 let handler = async (m, { conn }) => {
-    conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (!(id in conn.tebakgambar)) throw false
-    let json = conn.tebakgambar[id][1]
-    conn.sendButton(m.chat, '```' + json.jawaban.replace(/[bcdfghjklmnpqrstvwxyz]/ig, '_') + '```', author, null, [
-        ['nyerah', 'menyerah']
-    ], m)
+    if (!(id in conn.tebakbendera)) throw false
+    let json = conn.tebakbendera[id][1]
+    m.reply('```' + json.name.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```\nBalas soalnya Bukan Pesan ini_-')
 }
-handler.command = /^hint$/i
-
+handler.command = /^tebe$/i
 handler.limit = true
-
 export default handler
